0e27f37d6a8f4f04885c8f845434495f9ab479b2
surujmiah6896 <surujmiah6896@gmail.com>
2024-01-03 20:00:14 +0600

    add 404 not found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <!-- Include the Tailwind CSS stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gradient-to-br from-purple-500 via-pink-500 to-red-500 h-screen flex flex-col items-center justify-center">
+
+    <div class="text-center text-white">
+        <h1 class="text-6xl font-bold mb-2">404</h1>
+        <p class="text-2xl mb-4">Oops! Url Not Found</p>
+        <a href="/" class="bg-white text-purple-500 px-4 py-2 rounded-full hover:bg-purple-600 hover:text-white transition duration-300">Go back to Home</a>
+    </div>
+
+</body>
+</html>
